7389ec258146f96a3e9a684f725ce61d7e349804
chiehLiu <a22806869@gmail.com>
2021-04-22 00:52:40 +0800

    deploy

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <app-header></app-header>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+import showTitle from './components/showTitle.vue'
+import header from './components/header.vue'
+
+
+export default {
+    components: {
+        'add-blog':addBlog,
+        'show-blogs':showBlogs,
+        'show-title':showTitle,
+        'app-header': header
+    },
+    data () {
+        return {
+          
+        }
+    },
+    methods: {
+        }
+    }
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+   
+</style>
---
+++ src/components/showBlogs.vue
@@ -0,0 +1,71 @@
+<template>
+    <div v-theme:column="'narrow'"id="show-blogs">
+        <h1>All Blog Articles</h1>
+        <input type="text" v-model="search" placeholder="search blogs">
+        <div v-for="blog in filteredBlogs" class="single-blog">
+            <router-link v-bind:to="'/blog/' + blog.id"><h2>{{blog.title | to-uppercase}}</h2></router-link>
+            <article>{{blog.content | snippet}}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+import mixin from'../mixins/mixins'
+export default {
+    
+    data () {
+        return {
+          blogs: [],
+          search:''
+        }
+    },
+    methods: {
+        },
+    created(){
+        this.$http.get('https://vue-project-dc556-default-rtdb.firebaseio.com/posts.json').then(function(data){
+            return data.json();
+            
+        }).then(function(data){
+            var blogsArray = [];
+
+            for(let key in data){
+                // console.log(data[key]);
+                data[key].id = key 
+                blogsArray.push(data[key]);
+            }
+            console.log(blogsArray);
+            this.blogs = blogsArray;
+        })
+    },
+    computed: {
+        
+    },
+    filters:{
+        toUppercase(value){
+            return value.toUpperCase();
+        }
+    },
+    directives:{
+        'rainbow':{
+            bind(el, binding, vnode) {
+                el.style.color = `#${Math.random().toString().slice(2,8)}`
+            }
+        }
+    },
+    mixins:[mixin]
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width:800px;
+    margin:0 auto;
+}
+
+.single-blog{
+    padding:20px;
+    margin:20px 0;
+    box-sizing:border-box;
+    background:#eee;
+}
+</style>
